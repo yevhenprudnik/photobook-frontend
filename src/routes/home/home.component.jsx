b4a4728802bf2c9f6a0a06afec8afd579540b79a
Yevhen Prudnik <yevhenprudnik@gmail.com>
2023-11-27 23:14:38 +0200

    Final test.

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,12 +1,12 @@
-import { useContext } from 'react';
-import { UserContext } from '../../components/contexts/user.context';
+import { useContext } from "react";
+import { UserContext } from "../../components/contexts/user.context";
 
 const Home = () => {
   const { currentUser } = useContext(UserContext);
 
   return (
     <div>
-      Hello, World!
+      <h1>Hello, World!</h1>
       <br />
       {currentUser?.username}
     </div>
